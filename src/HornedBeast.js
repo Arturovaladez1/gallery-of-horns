b9ded6623566252631d0a473efc7a9dc1dfa3c98
Arturo Cunillas Valadez <Arturo.valadez1@aol.com>
2023-02-28 22:39:11 -0800

    not finished

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,17 +1,40 @@
 import React from 'react';
+import './Main.css';
+import Card from 'react-bootstrap/Card';
+import { Badge } from 'react-bootstrap';
 
 class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: 0
+    };
+  }
+
+  handleLikes = () => {
+    this.setState({
+      likes: this.state.likes + 1,
+    })
+  }
+
   render() {
-     console.log(this.props);
-    
+    console.log(this.props);
+
     return (
       <article>
-        <h2>{this.props.title}</h2>
-        <img src={this.props.imageUrl} alt={this.props.description} title={this.props.title}></img>
-        <p>{this.props.description}</p>
+        <card>
+          <h2>{this.props.title}</h2>
+          <p>👍{this.state.likes} <Badge bg="danger"></Badge>likes</p>
+          <Card.Img onClick={this.handleLikes} variant="top" src={this.props.imageUrl} alt={this.props.description} title={this.props.title} />
+          <Card.Body>
+            <Card.Text>
+              {this.props.description}
+            </Card.Text>
+          </Card.Body>
+        </card>
       </article>
-    );
-  }
-};
+    )
+  };
+}
 
 export default HornedBeast;
--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
+import './Main.css';
+
 
 class Main extends React.Component {
 
   render() {
+    console.log(this.props.data);
+    let horns = [];
+    this.props.data.forEach((horn, idx) => {
+      horns.push(
+        <HornedBeast
+          title={horn.name}
+          imageUrl={horn.image_url}
+          description={horn.description}
+          key={idx}
+        />
+      )
+
+    });
+
     return (
+
       <main>
-       <HornedBeast title={"LongHorn"} imageUrl={"https://images.unsplash.com/photo-1504450874802-0ba2bcd9b5ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"} description={"Longhorn has three horns over head."}/>
-       {/* <HornedBeast title={"LongHorn"} imageUrl={"https://images.unsplash.com/photo-1504450874802-0ba2bcd9b5ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"} description={"Longhorn has three horns over head."}/> */}
+        {horns}
       </main>
     )
   };
 }
 export default Main;
+
